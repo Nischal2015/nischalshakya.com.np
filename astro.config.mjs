ecea2d6b7d2421e34d490403ec2ec9facfd12938
Nischal Shakya <nischalshakya2015@gmail.com>
2023-04-08 13:03:14 +0545

    chore(package): update dependencies
    - Update 'astro' package to version 2.2.1, and '@astrojs/tailwind' to
    version 3.1.1.
    - Remove `@astrojs/mdx' package, as it is no longer needed
    - Add social image for open graph and twitter cards

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 import { astroImageTools } from 'astro-imagetools';
 
 export default defineConfig({
-  integrations: [tailwind(), mdx(), astroImageTools],
+  integrations: [tailwind(), astroImageTools],
 });
