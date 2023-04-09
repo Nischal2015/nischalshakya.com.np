0064218cdf6555fae9c5b6d01a0d9c0c449514fa
Nischal Shakya <nischalshakya2015@gmail.com>
2023-04-09 11:49:29 +0545

    chore: configure @astro/mdx package

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import { astroImageTools } from 'astro-imagetools';
+import mdx from "@astrojs/mdx";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools],
+  integrations: [tailwind(), astroImageTools, mdx()]
 });
