5a430aad68bd888ba9c4576cd4fcda62ee997a99
Nischal Shakya <nischalshakya2015@gmail.com>
2023-04-11 19:33:42 +0545

    chore(deps): update dependencies
    - remove 'astro-imagetools' and 'cosmisjs'
    - add '@astrojs/image' and 'sharp'
    - configure project to use 'sharp' for image processing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import { astroImageTools } from 'astro-imagetools';
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), astroImageTools, mdx()]
+  integrations: [
+    tailwind(),
+    mdx(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+  ],
 });
