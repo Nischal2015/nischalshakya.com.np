ef735061f299668ce93ac3272c5be7e6c7d0797c
Nischal Shakya <nischalshakya2015@gmail.com>
2023-04-20 19:21:54 +0545

    refactor(data): create separate data folder
    - add education data

---
+++ src/data/education.ts
@@ -0,0 +1,20 @@
+import type { AchievementProps } from '../types';
+
+export const educationList: AchievementProps[] = [
+  {
+    school: 'Tribhuvan University, IOE, Pulchowk Campus',
+    education: "Bachelor's Degree, Computer Engineering",
+    startMonth: 'Nov',
+    startYear: '2018',
+    endMonth: 'May',
+    endYear: '2023',
+  },
+  {
+    school: 'Moonlight Higher Secondary School',
+    education: 'High School Diploma, Physics',
+    startMonth: 'Jul',
+    startYear: '2016',
+    endMonth: 'Apr',
+    endYear: '2018',
+  },
+];
